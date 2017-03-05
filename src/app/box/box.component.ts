53add1d6184e3b99fe13e24b2dbfafac15c8efdd
Dmitry Troitskiy <Dmitriytro@gmail.com>
2017-03-05 20:47:07 +0300

    animation creating

--- src/app/box/box.component.ts
+++ src/app/box/box.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, trigger, state, animate, style, transition } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { SwipeService } from "../swipe.service";
 
 @Component({
@@ -8,19 +8,7 @@ import { SwipeService } from "../swipe.service";
   host: {
     '(document:keyup)': '_keyup($event)',
     '(document:keydown)': '_keydown($event)',
-  },
-  animations: [
-    trigger('movementtigger',[
-      state('fp',style({transform: 'translateX(0px)'})),
-      state('lp',style({transform: 'translateX(120px)'})),
-      transition('fp => lp',[
-        animate('1000ms ease-in')
-      ]),
-      transition('lp => fp',[
-        animate('1000ms ease-out')
-      ])
-    ])
-  ]
+  }
 })
 export class BoxComponent implements OnInit {
   rows = [0,1,2,3];
@@ -31,7 +19,12 @@ export class BoxComponent implements OnInit {
     null,null,null,null,
     null,null,4,8
   ];
-  state: string = 'fp';
+  animationList = [
+    'null','null','null','null',
+    'null','null','null','null',
+    'null','null','null','null',
+    'null','null','null','null'
+  ];
   constructor(private swipeService: SwipeService){}
   ngOnInit() {
     this.generate();
@@ -42,12 +35,12 @@ export class BoxComponent implements OnInit {
   _keyup(e): void{
     if(e.code == 'ArrowRight'){
       let newList = this.swipeService.swipeRight(this.sharedList);
-      this.state = 'lp';
+      this.animationList[3] = 'r3';
       //здесь устанавливаем анимацию при различных изменениях
       //нужен анализ каждых строк и огромная куча анимаций
       //найти решение для разделения и индификации каждого отдельного спота
       //тянуть анимацию до момента изменения и в конце сделать присвоение
-      // this.sharedList = newList;
+      this.sharedList = newList;
     }else if(e.code == 'ArrowLeft'){
       let newList = this.swipeService.swipeLeft(this.sharedList);
       this.sharedList = newList;
@@ -64,4 +57,40 @@ export class BoxComponent implements OnInit {
     e.preventDefault();
   }
 
+  /*
+  Promise
+  Promise созданны для организации асинхронного кода и  неявно используются в генераторах
+  Это обьект с 3 состояниями: 1) pending 2) fulfilled 3) rejected
+  можно навешивать колбэки 2 типов: onFulfilled и onRejected
+   var promise = new Promise(function(resolve, reject) {resolve() or reject()})
+   это содание нового обьекта и передача ему в аргументы функции которая будет выполнена сразу, и внутри будем менять
+   состояние в зависимости от успешности выполнения
+   в resolve и reject можно передать только 1 аргумент результата
+   promise.then(onFulfilled, onRejected)
+   .then принимает 2 функции, которые будут выполненны по завершению выполнения основного кода
+   promise.then(onFulfilled) //только успешный вариант
+   promise.then(null, onRejected) //только ошибка .catch(onRejected) или throw new Error("o_O");
+   обычно в reject передают объект ошибки
+
+   promise не может иметь обратного действия. Если произошел resolve, за ним не может идти reject
+
+   Http объект в angular2 возвращает Observable который мы можем переделать в Promise через .toPromise() оператор
+
+   Цепочка промисов работает по принципу, следующий then может идти если преведущая функция отработана или если она
+   возвращает promise, можно вернуть http запрос
+
+   Object Promise имеет св-ва:
+   PromiseState, PromiseResult, PromiseFulfillReactions, PromiseRejectReactions
+   меняется, при изменение состояния, вначале пуст, и две очереди колбэков
+
+   К одному обьекту можно привязать 2 then, и они выполнятся один, за другим
+
+   Promise.all(iterable) Ждём когда всё зарезолвится!
+   Promise.race(iterable) Ждём только первый промис!
+   Promise.resolve(value) Можем начать цепочку с value!
+   Promise.resolve(window.location) // начать с этого значения
+   .then(httpGet) // вызвать для него httpGet
+   .then(alert) // и вывести результат
+   Promise.reject(error) //аналоги
+   */
 }
--- src/app/spot/spot.component.ts
+++ src/app/spot/spot.component.ts
@@ -1,12 +1,29 @@
-import { Component, OnChanges, Input} from '@angular/core';
+import { Component, OnChanges, Input, trigger, state, animate, style, transition } from '@angular/core';
 
 @Component({
   selector: 'app-spot',
   templateUrl: './spot.component.html',
-  styleUrls: ['./spot.component.css']
+  styleUrls: ['./spot.component.css'],
+  animations: [
+    trigger('movementtigger',[
+      state('null',style({transform: 'translateX(0px)'})),
+      state('r3',style({transform: 'translateX(120px)'})),
+      transition('null => r3',[
+        animate('1000ms ease-in')
+      ]),
+      transition('r3 => null',[
+        animate('1000ms ease-out')
+      ])
+    ])
+  ]
 })
 export class SpotComponent implements OnChanges {
   @Input() number: Number;
+  @Input() anim: String;
+  state: String = 'null';
   constructor() { }
-  ngOnChanges(): void{}
+  ngOnChanges(): void{
+    this.state = this.anim;
+    console.log(this.state)
+  }
 }
